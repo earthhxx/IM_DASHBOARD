25b2b6d07b176d0910cf6e8798cbd7f7cf93f1dd
earthhxx <earthhwachirawit@gmail.com>
2025-05-29 10:31:15 +0700

    save

---
+++ src/app/api/temperature/fridge/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createConnection } from '../../../../lib/db';
+import sql from 'mssql';
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = req.nextUrl;
+    const num = searchParams.get('location');
+
+    if (!num) {
+        return NextResponse.json({ message: 'Missing location parameter' }, { status: 400 });
+    }
+
+    try {
+        const pool = await createConnection();
+        const result = await pool
+            .request()
+            .input('line', sql.NVarChar, `ตู้เย็น ${num}`)
+            .query(`
+                SELECT 
+                    [ID],
+                    [Date],
+                    [Doc_Name],
+                    [Area],
+                    [Line],
+                    [TMax1],  [TMax2],  [TMax3],  [TMax4],  [TMax5],  [TMax6],
+                    [TMax7],  [TMax8],  [TMax9],  [TMax10], [TMax11], [TMax12],
+                    [TMax13], [TMax14], [TMax15], [TMax16], [TMax17], [TMax18],
+                    [TMax19], [TMax20], [TMax21], [TMax22], [TMax23], [TMax24],
+                    [TMax25], [TMax26], [TMax27], [TMax28], [TMax29], [TMax30],
+                    [TMax31]
+                FROM 
+                    [DASHBOARD].[dbo].[Temperature]
+                WHERE 
+                    [Line] = @line;
+            `);
+
+        if (result.recordset.length === 0) {
+            return NextResponse.json({ message: 'No data found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ data: result.recordset });
+
+    } catch (error) {
+        console.error('Error fetching data:', error instanceof Error ? error.message : 'Unknown error');
+        return NextResponse.json(
+            { message: 'Internal Server Error', error: error instanceof Error ? error.message : 'Unknown error' },
+            { status: 500 }
+        );
+    }
+}
---
+++ src/app/api/temperature/superdry/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createConnection } from '../../../../lib/db';
+import sql from 'mssql';
+
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = req.nextUrl;
+    const num = searchParams.get('location');
+    if (!num ) {
+        return NextResponse.json({ message: 'Missing team or room parameter' }, { status: 400 });
+    }
+
+    try {
+        const pool = await createConnection();
+        const result = await pool
+            .request()
+            .input('num', sql.NVarChar, `ตู้ Supper Dry ${num}` )
+            .query(`           
+                    SELECT [ID], [Date], [Doc_Name], [Area], [Line],
+                                [HMax1], [HMax2], [HMax3], [HMax4], [HMax5], [HMax6], [HMax7], [HMax8], [HMax9], [HMax10],
+                                [HMax11], [HMax12], [HMax13], [HMax14], [HMax15], [HMax16], [HMax17], [HMax18], [HMax19], [HMax20],
+                                [HMax21], [HMax22], [HMax23], [HMax24], [HMax25], [HMax26], [HMax27], [HMax28], [HMax29], [HMax30],
+                                [HMax31]
+                        FROM [DASHBOARD].[dbo].[Temperature]
+                        WHERE Line = @num;
+            `);
+
+        // console.log(`Qurey`, result.recordset);
+
+        if (result.recordset.length === 0) {
+            return NextResponse.json({ message: 'No data found' }, { status: 404 });
+            // console.log(result.recordset)
+        }
+        return NextResponse.json({ data: result.recordset });
+
+    } catch (error) {
+        console.error('Error fetching data:', error instanceof Error ? error.message : 'Unknown error');
+        return NextResponse.json(
+            { message: 'Internal Server Error', error: error instanceof Error ? error.message : 'Unknown error' },
+            { status: 500 }
+        );
+    }
+}
+
+
+
+
+
