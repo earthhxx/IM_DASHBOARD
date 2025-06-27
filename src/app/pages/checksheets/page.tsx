8a7f41bfabfb44fa6311009fd0725f6ef431fce9
earthhxx <earthhwachirawit@gmail.com>
2025-06-27 17:22:38 +0700

    save

--- src/app/pages/checksheets/page.tsx
+++ src/app/pages/checksheets/page.tsx
@@ -1,7 +1,7 @@
 
 const checksheetsPages = () => {
     return (
-        <div className="flex-1 min-h-screen bg-white overflow-y-auto flex flex-col items-center font-extrabold justify-center font-bold  h-full w-full">
+        <div className="flex-1 min-h-screen bg-white overflow-y-auto flex flex-col items-center justify-center font-bold  h-full w-full">
             <h1 className="text-2xl font-bold">Check Sheets</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
                 <div className="">
